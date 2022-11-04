c6bafa11e1bd9e1179881a7ae4c9b267babb4ebb
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-11-04 12:54:20 +0000

    Fix slow running

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -9,7 +9,7 @@ always_ff @(posedge clk) begin
     if (rst) begin
         data_out <= 4'b1;
     end else if (en) begin
-        for (int i = 1; i <= 3; i--) begin
+        for (int i = 3; i >= 1; i--) begin
             data_out[i] <= data_out[i-1];
         end
         data_out[0] <= data_out[2] ^ data_out[3];
