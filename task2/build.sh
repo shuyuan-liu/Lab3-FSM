d5ed3df09c54d80181a1df3a4c19facb3dd68d7b
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-11-04 13:39:12 +0000

    Housekeeping for task2 folder

---
+++ task2/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+TOP_MODULE=f1_fsm
+
+set -e # Terminate script on any error
+
+rm -rf obj_dir
+rm -rf *.vcd
+
+verilator --top $TOP_MODULE -Wall --cc --trace *.sv --exe *_tb.cpp
+make -j7 -C obj_dir -f V$TOP_MODULE.mk
+./obj_dir/V$TOP_MODULE
