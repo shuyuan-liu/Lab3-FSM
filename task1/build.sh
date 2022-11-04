2277e8072b6184de0b8606a77ca91a473ecf7486
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-11-04 11:40:30 +0000

    Build automation

---
+++ task1/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+TOP_MODULE=lfsr
+
+set -e # Terminate script on any error
+
+rm -rf obj_dir
+rm -rf *.vcd
+
+verilator --top $TOP_MODULE -Wall --cc --trace *.sv --exe *_tb.cpp
+make -C obj_dir -f V$TOP_MODULE.mk
+./obj_dir/V$TOP_MODULE
