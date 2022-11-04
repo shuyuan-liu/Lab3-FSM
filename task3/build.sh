30ce6d8fc20d061edd2830b1681cbc363c6d6c4d
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-11-04 14:24:47 +0000

    Add testbench for auto F! light block
    The clkdiv required is ~40

--- task3/build.sh
+++ task3/build.sh
@@ -7,6 +7,6 @@ set -e # Terminate script on any error
 rm -rf obj_dir
 rm -rf *.vcd
 
-verilator --top $TOP_MODULE -Wall --cc --trace *.sv --exe *_tb.cpp
+verilator --top $TOP_MODULE -Wall --cc --trace *.sv --exe ${TOP_MODULE}_tb.cpp
 make -j7 -C obj_dir -f V$TOP_MODULE.mk
 ./obj_dir/V$TOP_MODULE
